368420396298e7ca56e405c46e0b5f522838a135
tberasley2 <tberasley@gmail.com>
2019-10-23 15:31:44 -0400

    Modified date and time

--- newAnnouncement.js
+++ newAnnouncement.js
@@ -28,7 +28,7 @@ function createAnnouncement(){
   var announcementBox = document.getElementById("announcementBox");
   var currentdate = new Date();
 
-  var datetime = "Last Sync: " + currentdate.getDate() + "/"
+  var datetime = currentdate.getDate() + "/"
                 + (currentdate.getMonth()+1)  + "/"
                 + currentdate.getFullYear() + " @ "
                 + currentdate.getHours()%12 + ":"
