f8a80ab2c7d49bd8187484a9397e6834d50a9d84
tberasley2 <tberasley@gmail.com>
2019-10-23 14:56:07 -0400

    Announcement page now looks much nicer with new bootstrap styling

--- newAnnouncement.js
+++ newAnnouncement.js
@@ -7,7 +7,12 @@ function newAnnouncement() {
   var createButton = document.createElement('button');
   var chooseFileButton = document.createElement('input');
 
-  contentTitle.setAttribute("type", "text");
+  var announcementForm =
+
+
+    "<div class=\"form-group\"><label for=\"title\">Title</label><input type=\"text\" class=\"form-control\" id=\"title\" placeholder=\"Enter Title\"></div><div class=\"form-group\"><label for=\"content\">Announcement</label><textarea class=\"form-control\" id=\"content\" placeholder=\"Enter Announcement\"></textarea></div><div class=\"form-group\"><label for=\"myFile\">Upload Photo</label><input type=\"file\" class=\"form-control-file\" id=\"myFile\"></div><input type=\"submit\" class=\"btn btn-primary\">Submit</button>";
+
+  /*contentTitle.setAttribute("type", "text");
   contentTitle.setAttribute("id", "title");
   contentTitle.setAttribute("value", "Title");
   contentTitle.setAttribute("class", "col-sm");
@@ -32,24 +37,21 @@ function newAnnouncement() {
 
   createButton.setAttribute("onclick", "createAnnouncement()");
   createButton.innerHTML = "Post";
-  createButton.setAttribute("class", "col-sm");
+  createButton.setAttribute("class", "btn btn-primary");
   var createButtonDiv = document.createElement('div');
   createButtonDiv.setAttribute("class", "row");
-  createButtonDiv.appendChild(createButton);
-
-
-
-
-
+  createButtonDiv.appendChild(createButton);-->
 
   box.appendChild(contentTitleDiv);
   box.appendChild(contentTextboxDiv);
   box.appendChild(chooseFileButtonDiv)
   box.appendChild(createButtonDiv);
+  while(newAnnouncementDiv.firstChild)
+    newAnnouncementDiv.removeChild(newAnnouncementDiv.firstChild);*/
+
+  newAnnouncementDiv.innerHTML = announcementForm;
 ;
 
-  while(newAnnouncementDiv.firstChild)
-    newAnnouncementDiv.removeChild(newAnnouncementDiv.firstChild);
 
 
 //  announcementBoxChildren.setAttribute("action","/action_page.php");
@@ -79,15 +81,16 @@ function createAnnouncement(){
 
   announcements.insertBefore(announcement, announcements.childNodes[0]);
 
-  while(announcementBox.firstChild) {
+  /*while(announcementBox.firstChild) {
     announcementBox.removeChild(announcementBox.firstChild);
-  }
+  }*/
 
+  newAnnouncementDiv.innerHTML = "";
   var newAnnouncementButton = document.createElement("button");
   newAnnouncementButton.setAttribute("id", "newAnnouncementButton");
   newAnnouncementButton.setAttribute("onclick", "newAnnouncement()");
   newAnnouncementButton.innerHTML = "Create Announcement"
 
   newAnnouncementDiv.appendChild(newAnnouncementButton);
-
+  return false;
 }
