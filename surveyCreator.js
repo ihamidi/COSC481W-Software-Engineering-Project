44fb890e005bc90c9486354251140282c8a818b5
tberasley2 <tberasley@gmail.com>
2019-10-25 12:30:47 -0400

    Added html and js files for the survey page

---
+++ surveyCreator.js
@@ -0,0 +1,15 @@
+function newSurvey() {
+  var newSurveyForm = document.getElementById("newSurveyForm");
+
+  newSurveyForm.innerHTML = "";
+
+}
+
+function newQuestion() {
+  var questionsDiv = document.getElementById("questions");
+
+}
+
+function newOptionMultipleChoice() {
+  
+}
