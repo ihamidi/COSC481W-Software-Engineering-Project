7831676b4f3fd5c1b48b0df9e6b71284babda338
ProgrammerSusan <hatems2017@gmail.com>
2019-10-25 20:06:00 -0400

    cleaned up code to account for static files

--- login_node/registration.js
+++ login_node/registration.js
@@ -1,19 +1,14 @@
 var mysql = require('mysql');
 var express = require('express');
-var bodyParser = require('body-parser');
 var path = require('path');
-var app = express();
-
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
+var app = express().configure(function () {
+    this.use('/public', express.static('public')); 
+});
 
 app.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname + '/registration.html'));
 });
 
-app.get('/download', function(req, res){
-    const file = (path.join(__dirname + '/test.pdf'));
-    res.download(file);
-  });
+app.use('/forms', express.static(path.join(__dirname, './test.pdf')));
 
 app.listen(3000);
