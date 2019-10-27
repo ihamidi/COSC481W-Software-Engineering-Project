bda9f3d93d5c41d73051490ca6ccd33e70a15a6d
ProgrammerSusan <hatems2017@gmail.com>
2019-10-27 08:02:37 -0400

    NodeJS upload button added

--- login_node/registration.js
+++ login_node/registration.js
@@ -1,7 +1,15 @@
-var mysql = require('mysql');
-var express = require('express');
-var path = require('path');
-var app = express();
+
+const express = require('express');
+const path = require('path');
+const bodyParser= require('body-parser')
+const http = require('http');
+const multer  = require('multer');
+const app = express();
+
+
+var renderPage = app.get('/', function (request, response) {
+	response.sendFile(path.join(__dirname + '/registration.html'));
+});
 
 app.set(function () {
     this.use('/public', express.static('public')); 
@@ -9,10 +17,26 @@ app.set(function () {
 
 app.use('/forms', express.static(path.join(__dirname, './test.pdf')));
 
-app.get('/', function (request, response) {
-	response.sendFile(path.join(__dirname + '/registration.html'));
-});
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+  })
+   
+  var upload = multer({ storage: storage })
 
-
+  app.post('/uploadfile', upload.single('RegistrationForm'), (req, res, next) => {
+    const file = req.file
+    if (!file) {
+      const error = new Error('Please upload a file')
+      error.httpStatusCode = 400
+      return next(error)
+    }
+      res.send(file)    
+      renderPage;
+  })
 
 app.listen(3000);
