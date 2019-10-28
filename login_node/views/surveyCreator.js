0e248c88943d1d458004355dc699868ec7071e53
tberasley2 <tberasley@gmail.com>
2019-10-28 18:42:14 -0400

    modified createsurvey.html

--- login_node/views/surveyCreator.js
+++ login_node/views/surveyCreator.js
@@ -7,14 +7,29 @@ function newSurvey() {
 
 function newQuestion() {
   var questionsDiv = document.getElementById("questions");
-  var questionsValue;
+
 
 
 
 }
 
-function newOptionMultipleChoice() {
+function newOptionTest() {
+  var responses = document.getElementById("qresponses1");
+  newOptionMultipleChoice(responses);
+}
 
+function newOptionMultipleChoice(responses) {
+  var newOption = document.createElement('div');
+  var lineBreak = document.createElement('br');
+  // <input id="qopt1-1" type="text" class="form-control" placeholder="Enter Answer...">
+  var responseInput = document.createElement('input');
+  responseInput.setAttribute("id","qopt1-1");
+  responseInput.setAttribute("type","text");
+  responseInput.setAttribute("class", "form-control");
+  responseInput.setAttribute("placeholder","Enter Answer...")
+  newOption.appendChild(responseInput);
+  newOption.appendChild(lineBreak);
+  responses.appendChild(newOption);
 }
 
 class Question {
@@ -30,7 +45,22 @@ class MultipleChoice extends Question {
   }
   template() {
     var template = document.createElment('div');
-    
+
   }
+}
+
+class Responses {
+  constructor(type) { // type is a 1-4 integer value for the type of question response. Ex: Multiple choice
+    var responseCount = 0;
+    this.type = type;
+    // initialize reponse type
+  }
+  // return repsonse type
+  // add new responses
+
+
+}
+
+
 
 }
