ff1c3baaa14aa36028177f579f157689df04bea5
ProgrammerSusan <hatems2017@gmail.com>
2019-12-14 16:39:21 -0500

    fixed button error

--- login_node/studentManagement.js
+++ login_node/studentManagement.js
@@ -53,7 +53,7 @@ var storage = multer.diskStorage({
       cb(null, 'uploads');
     },
     filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + req.session.studentName + req.session.studentLast) + req.session.studentID;
+      cb(null, file.fieldname + '-' + req.session.studentName + '_' + req.session.studentlastname);
     }
   })
 
@@ -100,8 +100,8 @@ var storage = multer.diskStorage({
        acctype: req.session.acctype,
        times: req.session.times,
        parentid: req.session.PID,
-       hasWaiver: req.session.hasWaiver,
-       hasPermission: true,
+       hasWaiver: true,
+       hasPermission: req.session.hasPermission,
        studentname: req.session.studentName,
        selectedstudent: req.session.selected,
        announcements: load_announcements()
